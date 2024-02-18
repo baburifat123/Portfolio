fe7b5c8532f037edb6bb431116d8a63eea4de9aa
baburifat078 <baburifat178@gmail.com>
2024-02-18 10:04:07 +0600

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import aboutImg from "../assets/images/say.png";
+const About = () => {
+  const info = [
+    { text: "Years experience", count: "03" },
+    { text: "Completed Projects", count: "24" },
+    { text: "Companies Work", count: "02" },
+  ];
+  return (
+    <section id="about" className="py-10 text-white">
+      <div className="text-center mt-8">
+        <h3 className="text-4xl font-semibold">
+          About <span className="text-cyan-600">Me</span>
+        </h3>
+        <p className="text-gray-400 my-3 text-lg">My introduction</p>
+        <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
+          <div className="p-2">
+            <div className="text-gray-300 my-3">
+              <p className="text-justify leading-7 w-11/12 mx-auto">
+                I'm a passionate front-end developer, weaving magic with code
+                and design to create seamless digital experiences. From crafting
+                elegant UIs to optimizing user interactions, I thrive on the
+                intersection of creativity and technology. With a keen eye for
+                detail and a commitment to user-centric design, I strive to make
+                the web a more beautiful and functional place, one line of code
+                at a time
+              </p>
+              <div className="flex mt-10 items-center gap-7">
+                {info.map((content) => (
+                  <div key={content.text}>
+                    <h3 className="md:text-4xl text-2xl font-semibold text-white">
+                      {content.count}
+                      <span className="text-cyan-600">+</span>{" "}
+                    </h3>
+                    <span className="md:text-base text-xs">{content.text}</span>
+                  </div>
+                ))}
+              </div>
+              <br />
+              <br />
+              <a href="../components/New folder/cv.png" download>
+                <button className="btn-primary">Download CV</button>
+              </a>
+            </div>
+          </div>
+          <div className="flex-1 md:mt-0 mt-6 flex justify-center items-center">
+            <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm aboutImg ">
+              <img
+                src={aboutImg}
+                alt=""
+                className="w-full object-cover bg-cyan-600 rounded-xl"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Project.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import project1 from "../assets/images/project-1.png";
+import project2 from "../assets/images/project-2.jpg";
+
+import project_person from "../assets/images/project_person1.png";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper";
+
+const Project = () => {
+  const projects = [
+    {
+      img: project1,
+      name: "Ecommerce",
+      github_link: "https://github.com/baburifat123/First-ecommerce-website",
+      live_link:
+        "https://65d0f6ee8f9229ac6eabbe03--cozy-cassata-dd03a1.netlify.app/",
+    },
+    {
+      img: project2,
+      name: "Simple Donation Web",
+      github_link: "https://github.com/baburifat123/Donation-Campaign",
+      live_link: "https://preeminent-zuccutto-9b1673.netlify.app/details/3",
+    },
+  ];
+  return (
+    <section id="projects" className="py-10 text-white">
+      <div className="text-center">
+        <h3 className="text-4xl font-semibold">
+          My <span className="text-cyan-600">Projects</span>
+        </h3>
+        <p className="text-gray-400 mt-3 text-lg">My awesome works</p>
+      </div>
+      <br />
+      <div className="flex max-w-6xl gap-6 px-5 mx-auto items-center relative">
+        <div className="lg:w-2/3 w-full">
+          <Swiper
+            slidesPerview={1.2}
+            spaceBetween={20}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+            }}
+            loop={true}
+            autoplay={{
+              delay: 3000,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination, Autoplay]}
+          >
+            {projects.map((project_info, i) => (
+              <SwiperSlide key={i}>
+                <div className="h-fit w-full p-4 bg-slate-700 rounded-xl">
+                  <img src={project_info.img} alt="" className="rounded-lg" />
+                  <h3 className="text-xl my-4">{project_info.name}</h3>
+                  <div className="flex gap-3">
+                    <a
+                      href={project_info.github_link}
+                      target="_blank"
+                      className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
+                    >
+                      Github
+                    </a>
+                    <a
+                      href={project_info.live_link}
+                      target="_blank"
+                      className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
+                    >
+                      Live Demo
+                    </a>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className="lg:block hidden">
+          <img src={project_person} alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Project;
